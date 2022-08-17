2518eab2345a1b9efeb758a6a46bbdc8e13f22c5
Andrea-gt <a.ximeram@gmail.com>
2022-08-17 01:29:13 -0600

    Camera Permission

--- app/src/main/java/com/example/laboratorio_4/DetailsActivity.kt
+++ app/src/main/java/com/example/laboratorio_4/DetailsActivity.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 
 class DetailsActivity : AppCompatActivity() {
@@ -20,6 +21,8 @@ class DetailsActivity : AppCompatActivity() {
     private lateinit var scheduleText: TextView
     private lateinit var phoneNumber: TextView
     private lateinit var appointmentButton: Button
+    private val CAMERA_PERMISSION_CODE = 12
+    private lateinit var rootLayout:ConstraintLayout;
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -32,6 +35,7 @@ class DetailsActivity : AppCompatActivity() {
         nameText = findViewById(R.id.restaurantName)
         directionsText = findViewById(R.id.restaurantDirections)
         scheduleText = findViewById(R.id.restaurantSchedule)
+        rootLayout = findViewById(R.id.rootConstraint)
 
         val data: Data = intent.getSerializableExtra("restaurantInformation") as Data
 
@@ -50,9 +54,49 @@ class DetailsActivity : AppCompatActivity() {
             startActivity(intent)
         }
         appointmentButton.setOnClickListener {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 0)
+            checkPermission(Manifest.permission.CAMERA,CAMERA_PERMISSION_CODE)
         }
     }
 
+    private fun checkPermission(permission:String,requestCode:Int){
+        if(ContextCompat.checkSelfPermission(this,permission)==PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, arrayOf(permission),requestCode)
+        } else{
+            Toast.makeText(this,"Permiso otorgado",Toast.LENGTH_SHORT).show()
+        }
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==CAMERA_PERMISSION_CODE){
+            if(grantResults.isNotEmpty()&& grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Permiso de Camara Otorgado", Toast.LENGTH_SHORT).show()
+            } else{
+                checkRequestRationale()
+            }
+        }
+    }
+
+    private fun checkRequestRationale() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                val snackbar = Snackbar.make(
+                    rootLayout,
+                    "Acceso a cámara es necesario para poder tomar fotos",
+                    Snackbar.LENGTH_INDEFINITE
+                )
+                snackbar.setAction("Ok"){
+
+                    checkPermission(Manifest.permission.CAMERA,CAMERA_PERMISSION_CODE)
+                }
+                snackbar.show()
+            } else {
+                checkPermission(Manifest.permission.CAMERA,CAMERA_PERMISSION_CODE)
+            }
+        }
+    }
 }
